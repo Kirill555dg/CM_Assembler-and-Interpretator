10506b0b12d65954c16fe0cd9ff67408ccb60dbf
Kerrodar <Kirill555dg@gmail.com>
2024-11-18 00:27:07 +0300

    add executable script

--- script.py
+++ script.py
@@ -26,35 +26,47 @@ def assemble():
         exit(1)
 
     assembler = Assembler(path_to_code, path_to_binary_file, path_to_log)
-    assembler.assemble()
+    try:
+        assembler.assemble()
+    except ValueError as e:
+        print(e)
+        exit(1)
+
     assembler.to_binary_file()
 
 
 def interpret():
     if len(argv) < 4:
-        print('Введены не все аргументы для корректной работы ассемблера')
-        return
+        print('Введены не все аргументы для корректной работы интерпретатора')
+        exit(1)
 
-    path_to_bin = argv[2]
-    limitation = argv[3]
+    path_to_binary_file = argv[2]
+    left_boundary = argv[3]
+    right_boundary = argv[4]
 
-    if not exists(path_to_bin):
-        print('Файла с таким именем не существует')
-        return
+    if not exists(path_to_binary_file):
+        print('Бинарный файл не найден')
+        exit(1)
 
     try:
-        limitation = int(limitation)
+        left_boundary, right_boundary = int(left_boundary), int(right_boundary)
     except ValueError:
-        print('Ограничение должно быть задано целым числом')
-        return
+        print('Границы диапазона памяти УВМ должны быть заданы целыми числами')
+        exit(1)
 
-    my_interpreter = Interpreter(path_to_bin, limitation)
-    my_interpreter.interpret()
+    interpreter = Interpreter(path_to_binary_file, left_boundary, right_boundary)
+
+
+    try:
+        interpreter.interpret()
+    except ValueError as e:
+        print(e)
+        exit(1)
 
 
 if __name__ == '__main__':
     if len(argv) < 2:
-        print('Не введено имя подпрограммы для исполнения')
+        print('Для запуска скрипта необходимо ввести assemble или interpret с соответствующими аргументами')
     match argv[1]:
         case 'assemble':
             assemble()
